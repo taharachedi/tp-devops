957c5d67df39a35a4c86d49306d53b79ae24a4b3
Taha Rachedi <taharachedi7@gmail.com>
2026-02-20 14:56:27 +0100

    Ajout app Node + Docker + Docker Compose

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, TP DevOps !');
+});
+
+app.listen(port, () => {
+    console.log('Serveur lancé sur http://localhost:' + port);
+});
